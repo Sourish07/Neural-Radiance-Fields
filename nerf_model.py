de64cfa6253023eb60ce7de5af9c6679022a6339
Sourish Kundu <sourishkundu07@gmail.com>
2024-06-23 15:44:16 -0700

    Added positional encoding

--- nerf_model.py
+++ nerf_model.py
@@ -4,10 +4,11 @@ import torch.nn.functional as F
 
 
 class TinyNerfModel(nn.Module):
-    def __init__(self, W=256):
+    def __init__(self, W=256, L_embed=6):
         super().__init__()
         
-        input_size = 3
+        self.L_embed = L_embed
+        input_size = 3 + 2 * 3 * L_embed
         
         self.block1 = nn.Sequential(
             nn.Linear(input_size, W),
@@ -40,11 +41,21 @@ class TinyNerfModel(nn.Module):
         )
     
 
+    def posenc(self, x):
+        output = [x]
+        for i in range(self.L_embed):
+            output.append(torch.sin(2.0 ** i * x))
+            output.append(torch.cos(2.0 ** i * x))
+
+        return torch.cat(output, dim=-1)
+
+
     def forward(self, inputs):
         """
             inputs: (batch_dim, 3)
             returns: RGB -> (batch_dim, 3), Sigma -> (batch_dim, 1)
         """
+        inputs = self.posenc(inputs)
         a = self.block1(inputs)
         b = self.block2(torch.cat([a, inputs], dim=-1))
         return  F.sigmoid(b[..., :3]), F.relu(b[...,3])
