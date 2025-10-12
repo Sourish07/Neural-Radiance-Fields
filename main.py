405ca09ac74ebac26b4a436bd1d182674d5a52a4
Sourish Kundu <sourishkundu07@gmail.com>
2025-10-12 16:00:07 -0700

    added wandb

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import os
 import time
 
-import matplotlib.pyplot as plt
 import torch
 from tqdm import tqdm
+import wandb
+from uuid import uuid4
+import tempfile
 
 from nerf_dataset import TinyCybertruckDataset
 from nerf_functions import get_device, get_rays, render_rays
@@ -12,17 +13,25 @@ from utils import init_xavier_uniform
 
 torch.set_float32_matmul_precision("high")
 
+JOB_ID = str(uuid4())
 SEED = 5
 LEARNING_RATE = 5e-4
 NUM_EPOCHS = 10
 NUM_SAMPLES_PER_RAY = 64
 BATCH_SIZE = 1
 
-output_dir = "output"
-timestamp = time.strftime("%Y%m%d-%H%M%S")
-checkpoint_dir = os.path.join(output_dir, timestamp)
-
-os.makedirs(checkpoint_dir, exist_ok=True)
+run = wandb.init(
+    project="nerf-cybertruck",
+    config={
+        "model": "tiny_nerf_model",
+        "dataset": "tiny_cybertruck_dataset",
+        "learning_rate": LEARNING_RATE,
+        "batch_size": BATCH_SIZE,
+        "seed": SEED,
+        "num_epochs": NUM_EPOCHS,
+        "num_samples_per_ray": NUM_SAMPLES_PER_RAY,
+    },
+)
 
 # Setting seed because model is sensitive to initialization
 torch.manual_seed(SEED)
@@ -55,7 +64,6 @@ near, far = train_data.get_near_far()
 H, W = train_data.get_image_size()
 
 for i in range(NUM_EPOCHS):
-    print(f"Epoch {i}")
     start_time = time.time()
 
     for target_image, pose, focal in tqdm(train_dataloader):
@@ -74,13 +82,27 @@ for i in range(NUM_EPOCHS):
         loss.backward()
         optimizer.step()
 
-    print(f"Epoch took {time.time() - start_time} seconds, Loss: {loss.item()}")
+        run.log({"loss": loss.item()})
+
     with torch.no_grad():
         rays_o, rays_d = get_rays(H, W, testfocal, testpose, device=device)
         rgb = render_rays(
             model, rays_o, rays_d, near, far, NUM_SAMPLES_PER_RAY, device=device
         )
-        plt.imshow(rgb.cpu().numpy())
-        plt.show()
+        img = rgb.mul(255).clamp(0, 255).to(torch.uint8).cpu().numpy()
+        run.log({"generated_images": [wandb.Image(i) for i in img]})
 
-    torch.save(model.state_dict(), os.path.join(checkpoint_dir, f"model_{i}.pt"))
+    with tempfile.NamedTemporaryFile(suffix=".pt") as tmp_file:
+        file_name = tmp_file.name
+        torch.save(model.state_dict(), file_name)
+        model_artifact = wandb.Artifact(
+            f"{run.id}_nerf_model.pt",
+            type="model",
+            description=f"Epoch {i + 1}",
+        )
+        model_artifact.add_file(file_name, skip_cache=True)
+        run.log_artifact(model_artifact)
+
+    run.log({"epoch_time": time.time() - start_time})
+
+run.finish()
